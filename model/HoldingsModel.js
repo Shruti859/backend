069ab606317b21cff9055eddf3e6c003dd4ef47c
princepandeyji <153540182+princepandeyji@users.noreply.github.com>
2025-04-30 13:47:13 +0530

    Initial commit

---
+++ model/HoldingsModel.js
@@ -0,0 +1,10 @@
+// const { model } = require("mongoose");
+const mongoose = require("mongoose");
+
+const {HoldingsSchema} =require("../schemas/HolidingsSchema")
+const HoldingsModel = mongoose.model("holding", HoldingsSchema); // ✅ Correct Model Creation
+
+
+// const HoldingsModel = new model("holding", HoldingsSchema);
+
+module.exports = { HoldingsModel };
